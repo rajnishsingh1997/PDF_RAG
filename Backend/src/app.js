988da9c3fe39ae07dedf24056d80af5acc286796
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2026-01-06 22:57:51 +0530

    global error handler added

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
 import connectToDatabase from "./config/mongodb.js";
+import globalErrorHandler from "../middleware/globalErrorHandler.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use("/api/auth", authRouter);
+app.use(globalErrorHandler);
 
 connectToDatabase()
   .then(() => {
--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,7 +1,14 @@
 import express from "express";
 const authRouter = express.Router();
-
-
-
-
+authRouter.post("/login", (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      throw new Error("Email and password are required");
+    }
+  } catch (error) {
+    console.log(error.message);
+    next(error);
+  }
+});
 export default authRouter;
