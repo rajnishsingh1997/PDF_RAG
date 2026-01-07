29e8d9a391180abfcefe064030a134c94fefc257
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2026-01-07 12:44:32 +0530

    added login logic here

--- Backend/src/service/loginService.js
+++ Backend/src/service/loginService.js
@@ -1,5 +1,24 @@
+import bcrypt from "bcryptjs";
+import User from "../models/userSchema.js";
+
 const loginService = async (email, password) => {
-    
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return { error: "Invalid credentials" };
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return { error: "Invalid credentials" };
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    return { message: "Login successful", token };
+  } catch (error) {
+    console.log("Error in loginService:", error);
+    throw error;
+  }
 };
 
 export default loginService;
