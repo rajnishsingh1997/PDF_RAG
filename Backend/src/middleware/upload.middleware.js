76804cbec99aaffaa0f77722c20745355aea086c
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2026-01-08 21:53:57 +0530

    refactoring

---
+++ Backend/src/middleware/upload.middleware.js
@@ -0,0 +1,27 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024,
+  },
+}).single("uploadedFile");
+
+const uploadFileMulterMiddleware = (req, res, next) => {
+  console.log("Inside upload middleware");
+  upload(req, res, (err) => {
+    if (err) {
+      console.error(err);
+      return next(err);
+    }
+    if (!req.file) {
+      return next(new Error("No file uploaded"));
+    }
+    console.log(req.file);
+    next();
+  });
+};
+
+export default uploadFileMulterMiddleware;
--- Backend/src/routes/upload.route.js
+++ Backend/src/routes/upload.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import verifyToken from "../middleware/verifyToken.js";
 import uploadController from "../controller/uploadController.js";
+import uploadFileMulterMiddleware from "../middleware/upload.middleware.js";
 
 const uploadRouter = express.Router();
 
-uploadRouter.post("/docs", verifyToken, uploadController);
+uploadRouter.post("/docs", verifyToken,uploadFileMulterMiddleware, uploadController);
 
 export default uploadRouter;
