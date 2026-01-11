58fc85c21ee4ffe82714ed939f323a276861ed93
Rajnish. R. Singh Rajput <srajnish235.rs@gmail.com>
2026-01-11 17:32:44 +0530

    langchain ingestion in progress

--- Backend/src/worker/ingestionWorker.js
+++ Backend/src/worker/ingestionWorker.js
@@ -1,5 +1,9 @@
 import Document from "../models/documentSchema.js";
 import AWS from "aws-sdk";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import fs from "fs";
+import os from "os";
+import path from "path";
 
 AWS.config.update({
   region: "ap-south-1",
@@ -9,17 +13,30 @@ AWS.config.update({
 const s3 = new AWS.S3();
 
 const injectionWorker = async (documentId) => {
-  const document = await Document.findOne({ documentId });
-  if (!document) {
-    throw new Error("Document not found in ingestion worker");
+  try {
+    const document = await Document.findOne({ documentId });
+    if (!document) {
+      throw new Error("Document not found in ingestion worker");
+    }
+    const { userId, s3Key } = document;
+    const downloadParams = {
+      Key: s3Key,
+      Bucket: "pdf-rag-storage",
+    };
+    const downloadedFile = await s3.getObject(downloadParams).promise();
+    if (!downloadedFile || !downloadedFile.Body) {
+      throw new Error("Failed to download file from S3");
+    }
+    const tempFilePath = path.join(os.tmpdir(), `${documentId}.pdf`);
+
+    fs.writeFileSync(tempFilePath, downloadedFile.Body);
+    const loader = new PDFLoader(tempFilePath);
+    const docs = await loader.load();
+    console.log(docs);
+  } catch (error) {
+    console.log("Error in ingestionWorker:", error);
+    throw error;
   }
-  const { userId, s3Key } = document;
-  const downloadParams = {
-    Key: s3Key,
-    Bucket: 'pdf-rag-storage',
-  };
-  const downloadedFile = await s3.getObject(downloadParams).promise();
-  console.log(downloadedFile.Body);
 };
 
 export default injectionWorker;
